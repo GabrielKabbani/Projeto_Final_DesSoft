34fd351da374fd6ec19cbee157b837005edef608
Luis Filipe Carrete <lfcsanchez@live.com>
2019-05-03 07:47:24 -0300

    Criando os arquivos

---
+++ Projeto Final.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  3 07:31:59 2019
+
+@author: lfcsa
+"""
+
+#Imports
+import pygame
+from os import path
+
+
+img_dir = path.join(path.dirname(__file__), 'img')
+snd_dir = path.join(path.dirname(__file__), 'snd')
+
