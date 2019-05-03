90adf603dcbff5992f559bf60b4a00ea62138b61
Luis Filipe Carrete <lfcsanchez@live.com>
2019-05-03 08:28:02 -0300

    Criei classe de player

--- Projeto Final.py
+++ Projeto Final.py
@@ -8,8 +8,105 @@ Created on Fri May  3 07:31:59 2019
 #Imports
 import pygame
 from os import path
+import time
 
 
-img_dir = path.join(path.dirname(__file__), 'img')
-snd_dir = path.join(path.dirname(__file__), 'snd')
+img_dir = path.join(path.dirname(__file__), 'img_dir')
+snd_dir = path.join(path.dirname(__file__), 'snd_dir')
 
+#Dados gerais do jogo
+WIDTH = 400
+HEIGHT = 600
+FPS = 60
+
+# Define algumas variáveis com as cores básicas
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
+#Inicia pygame
+
+pygame.init()
+pygame.mixer.init()
+
+#Cria assets
+
+def load_assets (img_dir, snd_dir):
+    assets = {}
+    assets['player_img'] = pygame.image.load(path.join(img_dir,"player_1.png")).convert()
+    return assets
+
+
+#Classes
+    
+#Classe player
+class Player(pygame.sprite.Sprite):
+    #Construtor de classe
+    def __init__ (self, player_img):
+        #Construtor de classe pai
+        pygame.sprite.Sprite.__init__(self)
+        
+        #Cria sprite
+        self.image = player_img
+        
+        #Define tamanho
+        self.image = pygame.transform.scale(player_img,(100,125))
+        
+        #Deixa transparente
+        self.image.set_colorkey(BLACK)
+        
+        #Detalhe sobre posicionamento
+        self.rect = self.image.get_rect()
+        
+        #Centraliza no baixo da tela 
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 20
+        
+        
+#Tamanho da tela
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+
+#Display nome do jogo
+pygame.display.set_caption("Ride Along")
+        
+#Variavel de relogio:
+clock = pygame.time.Clock()
+
+#Carrega os assets 
+assets = load_assets(img_dir, snd_dir)
+
+#Cria a variavel que contem classe do player
+player = Player(assets['player_img'])
+
+#Adiciona sprite 
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
+    
+
+#Para nao travarmos:
+try:
+    
+    running = True
+    while running:
+        #Ajusta velocidade de jogo
+        clock.tick(FPS)
+        
+        # Processa os eventos
+        for event in pygame.event.get():
+            
+            # Verifica se foi fechado
+            if event.type == pygame.QUIT:
+                running = False
+                
+        #Atualiza sprites depois de cada evento
+        all_sprites.update()
+        
+        
+        #Cada loop redesenha os sprites
+        all_sprites.draw(screen)
+        
+finally:
+    pygame.quit()
