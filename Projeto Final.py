8cd79da584d91ab551cfe6be53d37696abdbf4a2
Luis Filipe Carrete <lfcsanchez@live.com>
2019-05-03 09:10:22 -0300

    Adicionei o carro e movimento a ele

--- Projeto Final.py
+++ Projeto Final.py
@@ -37,6 +37,7 @@ pygame.mixer.init()
 def load_assets (img_dir, snd_dir):
     assets = {}
     assets['player_img'] = pygame.image.load(path.join(img_dir,"player_1.png")).convert()
+    assets['background'] = pygame.image.load(path.join(img_dir,"Background.png")).convert()
     return assets
 
 
@@ -53,7 +54,7 @@ class Player(pygame.sprite.Sprite):
         self.image = player_img
         
         #Define tamanho
-        self.image = pygame.transform.scale(player_img,(100,125))
+        self.image = pygame.transform.scale(player_img,(40,55))
         
         #Deixa transparente
         self.image.set_colorkey(BLACK)
@@ -63,7 +64,18 @@ class Player(pygame.sprite.Sprite):
         
         #Centraliza no baixo da tela 
         self.rect.centerx = WIDTH / 2
-        self.rect.bottom = HEIGHT - 20
+        self.rect.bottom = HEIGHT - 40
+        
+        #Velocidade 
+        self.speedx = 0
+        self.speedy = 0
+    def update(self):
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        
+        #Deixa dentro da tela
+        if self.rect.right > WIDTH - 55 or self.rect.left < 55:
+            self.speedx = 0
         
         
 #Tamanho da tela
@@ -77,6 +89,10 @@ clock = pygame.time.Clock()
 
 #Carrega os assets 
 assets = load_assets(img_dir, snd_dir)
+
+#Carrega fundo
+background = assets["background"]
+background_rect = background.get_rect()
 
 #Cria a variavel que contem classe do player
 player = Player(assets['player_img'])
@@ -101,12 +117,31 @@ try:
             if event.type == pygame.QUIT:
                 running = False
                 
+            #Verifica se clica tecla
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    player.speedx = -10
+                if event.key == pygame.K_RIGHT:
+                    player.speedx = 10
+            
+            #Verifica se tecla soltou
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    player.speedx = 0
+                if event.key == pygame.K_RIGHT:
+                    player.speedx = 0
+                
         #Atualiza sprites depois de cada evento
         all_sprites.update()
         
         
         #Cada loop redesenha os sprites
+        screen.fill(BLACK)
+        screen.blit(background, background_rect)
         all_sprites.draw(screen)
+        
+        #Depois de desenhar inverte o display
+        pygame.display.flip()
         
 finally:
     pygame.quit()
