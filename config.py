1b7eab9976c4bb83df3a2e13508445e575b5e301
Luis Filipe Carrete <lfcsanchez@live.com>
2019-05-15 11:25:57 -0300

    Comecei a criar tela inicial

---
+++ config.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 15 11:08:00 2019
+
+@author: lfcsa
+"""
+
+from os import path
+
+# Estabelece a pasta que contem as figuras e sons.
+img_dir = path.join(path.dirname(__file__), 'img_dir')
+snd_dir = path.join(path.dirname(__file__), 'snd_dir')
+fnt_dir = path.join(path.dirname(__file__), 'font')
+
+# Dados gerais do jogo.
+WIDTH = 400 # Largura da tela
+HEIGHT = 600 # Altura da tela
+FPS = 60 # Frames por segundo
+road_speed = 3 #Velocidade do carro
+
+# Define algumas variáveis com as cores básicas
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
+# Estados para controle do fluxo da aplicação
+INIT = 0
+GAME = 1
+QUIT = 2
